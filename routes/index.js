2c484879d77af9f872ad9b9e4501d077bac3d7ce
cou23001 <cou23001@byui.edu>
2024-01-12 21:32:15 -0700

    Connect to MongoDB

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
-let router = require('express').Router();
+const router = require('express').Router();
+const users = require('./users');
+
 
 router.get('/', (req,res) => {
     res.send("Hello World!");
 });
+
+
+router.use('/users', users);
 
 module.exports = router; 
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes/index');
+const mongodb = require('./data/database')
 
 const port = process.env.port || 3000;
 
 app.use('/', routes);
 
-app.listen(port, () => { 
-    console.log(`Running on port ${port}`)
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err)
+    } else {
+        app.listen(port, () => { 
+            console.log(`Database is listening and node running on port ${port}`)
+        });
+    }
 });
+
